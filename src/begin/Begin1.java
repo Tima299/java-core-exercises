c4460d61dbc327329a7eb2ad616f36c21b3c4710
Tima299 <117558961+Tima299@users.noreply.github.com>
2025-02-12 21:01:28 +0500

    Third line added

--- src/begin/Begin1.java
+++ src/begin/Begin1.java
@@ -4,5 +4,6 @@ public class Begin1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Second line");
+        System.out.println("Third line");
     }
 }
