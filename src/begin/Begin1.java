3e622dd057a297d666f149452cc7d25c9157fd96
Tima299 <117558961+Tima299@users.noreply.github.com>
2025-02-12 21:00:22 +0500

    Second line added

--- src/begin/Begin1.java
+++ src/begin/Begin1.java
@@ -3,5 +3,6 @@ package begin;
 public class Begin1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Second line");
     }
 }
